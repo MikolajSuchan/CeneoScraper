2d80dbbc60facac1415621ffee7b67f7f70d1a04
MikołajSuchan <225887@student.uek.krakow.pl>
2023-04-05 16:21:00 +0200

    2023-04-05 MS

--- scraper.py
+++ scraper.py
@@ -1,8 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+
 #product_code=input("Podaj kod produktu: ")
+
 product_code="96693065"
 url=f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response=requests.get(url)
 page_dom=BeautifulSoup(response.text,"html.parser")
-print(page_dom.prettify())
+opinions=page_dom.select("div.js_product-review")
+all_opinions=[]
+
+for opinion in opinions:
+    single_opinion={
+        "opinion_id":opinion["data-entry-id"],
+        "author":opinion.select_one("span.user-post__author-name").text.strip(),
+        "recomendation":opinion.select_one("span.user-post__author-recomedation>em").text.strip(),
+        "rating":opinion.select_one("span.user-post__score-count").text.strip(),
+        "verified":opinion.select_one("div.review-pz").text.strip(),
+        "post_date":opinion.select_one("span.user-post_published>time:nth-child(1)[")["datatime"].strip(),
+        "purchase_date":opinion.select_one("span.user-post_published>time:nth-child(2)[")["datatime"].strip(),
+        "vote_up":opinion.select_one("").text.strip(),
+        "vote_down":opinion.select_one("").text.strip(),
+        "content":opinion.select_one("").text.strip(),
+        "cons":opinion.select_one("").text.strip(),
+        "pros":opinion.select_one("").text.strip(),
+    }
+    print(type(opinion))
